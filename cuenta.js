5a5768c76ddb37ed20a10a6c1b08e15c092d738c
Facundo Ciruzzi <121110001+Facundo-c-c@users.noreply.github.com>
2024-07-22 16:53:08 -0300

    Add files via upload

--- cuenta.js
+++ cuenta.js
@@ -5,7 +5,7 @@ var indice = 0;
 
 function precio_tabla(num) {
 
-    switch (num) {
+    switch (num) { //suma al precio total segun la hamburgesa
         case 1: tipo_hambur = "Hamburguesa simple";
             precio_hambur = 10;
             monto_total =  monto_total + precio_hambur; break;
@@ -20,7 +20,7 @@ function precio_tabla(num) {
 
     indice++
 
-    let lugar_tbb = document.getElementById("t_body");
+    let lugar_tbb = document.getElementById("t_body"); //voy creando los elementos de la tabla
 
     let elemento_tr = document.createElement("tr");
     elemento_tr.id = "tr" + indice;
@@ -36,12 +36,28 @@ function precio_tabla(num) {
     elmento_td2.innerHTML = precio_hambur + "$";
     fila.appendChild(elmento_td2);
 
-    let pecio_total = document.getElementById("boton_precio");
-    pecio_total.innerHTML = "Pagar " + monto_total + "$";
+    let precio_total = document.getElementById("boton_precio");
+    precio_total.innerHTML = "Pagar " + monto_total + "$";
 }
 
-function confirmar(yes){
+function confirmar(yes){ //recargar pagina
     if(yes){
         location.reload();
     }
+}
+
+function recalcular_precio(){ //recalculo precio con o sin impuesto
+
+    let medio_pago = document.getElementById("mediopago");
+    let precio_total = document.getElementById("boton_precio");
+    let precio_calculo;   
+    console.log(medio_pago.checked)
+
+    if (medio_pago.checked == true){
+        precio_calculo = monto_total * 1.50;
+        precio_total.innerHTML = "Pagar " + precio_calculo + "$";
+    } else{
+        precio_total.innerHTML = "Pagar " + monto_total + "$";
+    }
+    precio_calculo = 0;
 }
